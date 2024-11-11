19a668bd2d348e757d672ac9f6ea64dd13985162
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-11 17:08:26 +0000

    Update GameDownloader.sh

--- GameDownloader.sh
+++ GameDownloader.sh
@@ -1,42 +1,40 @@
 #!/bin/bash
 
-# Function to display the main menu
-show_main_menu() {
-    local choice
-    dialog --clear --title "Game Downloader Menu" \
-    --menu "Please choose an option:" 15 50 2 \
-    1 "PSX Downloader" \
-    2 "Dreamcast Downloader" \
-    2>&1 >/dev/tty
-}
+# Define paths to the downloader menu scripts
+PSX_DOWNLOADER="/userdata/system/game-downloader/psx-downloader-menu.sh"
+DC_DOWNLOADER="/userdata/system/game-downloader/dc-downloader-menu.sh"
 
-# Function to run PSX Downloader
-run_psx_downloader() {
-    if [ -f "/userdata/system/game-downloader/psx-downloader-menu.sh" ]; then
-        /userdata/system/game-downloader/psx-downloader-menu.sh
-    else
-        dialog --msgbox "PSX Downloader script not found!" 6 40
-    fi
-}
+# Display main game downloader menu
+choice=$(dialog --clear --title "Game Downloader" \
+--menu "Choose a game downloader:" 15 50 2 \
+1 "PSX Downloader" \
+2 "Dreamcast Downloader" \
+3>&1 1>&2 2>&3)
 
-# Function to run Dreamcast Downloader
-run_dreamcast_downloader() {
-    if [ -f "/userdata/system/game-downloader/dc-downloader-menu.sh" ]; then
-        /userdata/system/game-downloader/dc-downloader-menu.sh
-    else
-        dialog --msgbox "Dreamcast Downloader script not found!" 6 40
-    fi
-}
-
-# Main menu loop
-while true; do
-    choice=$(show_main_menu)
-
+# Check if the user made a choice
+if [ $? -eq 0 ]; then
     case $choice in
-        1) run_psx_downloader ;;
-        2) run_dreamcast_downloader ;;
+        1)
+            # Run the PSX downloader menu if it exists
+            if [ -f "$PSX_DOWNLOADER" ]; then
+                bash "$PSX_DOWNLOADER"
+            else
+                dialog --msgbox "PSX Downloader script not found." 6 40
+            fi
+            ;;
+        2)
+            # Run the Dreamcast downloader menu if it exists
+            if [ -f "$DC_DOWNLOADER" ]; then
+                bash "$DC_DOWNLOADER"
+            else
+                dialog --msgbox "Dreamcast Downloader script not found." 6 40
+            fi
+            ;;
         *)
-            dialog --msgbox "Invalid option!" 6 40
+            dialog --msgbox "Invalid option." 6 40
             ;;
     esac
-done
+else
+    # Clear screen and exit if the user canceled
+    clear
+fi
