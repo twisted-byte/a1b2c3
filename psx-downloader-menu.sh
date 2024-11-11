42fdbafbac0ea7d0318871c41d2809f8dfd06b9e
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-11 19:16:38 +0000

    Update psx-downloader-menu.sh

--- psx-downloader-menu.sh
+++ psx-downloader-menu.sh
@@ -8,9 +8,9 @@ show_loading_message() {
 
 # Load the list of games into memory once when the script starts
 load_psx_games() {
-    # Show "Loading games list" message asynchronously
+    # Show the loading message in the background while the list is being loaded
     show_loading_message
-    
+
     # Read psx-links.txt and prepare the list of games in memory
     if [ -f "/userdata/system/game-downloader/psx-links.txt" ]; then
         mapfile -t games < /userdata/system/game-downloader/psx-links.txt
@@ -18,8 +18,8 @@ load_psx_games() {
         dialog --msgbox "psx-links.txt not found!" 6 40
         exit 1
     fi
-    
-    # Close the dialog once the game list is loaded
+
+    # Once the games are loaded, kill the background loading message
     kill $!  # Kill the background dialog process
 }
 
@@ -28,7 +28,7 @@ show_psx_menu() {
     # Create a menu list for the dialog menu
     local menu=()
     local idx=1
-    
+
     # Process the game list into a format suitable for dialog
     for game in "${games[@]}"; do
         game_name=$(echo "$game" | cut -d ' ' -f 1)
@@ -36,17 +36,17 @@ show_psx_menu() {
         menu+=("$idx" "$game_name")
         ((idx++))
     done
-    
+
     # Show the menu
     choice=$(dialog --clear --title "PSX Downloader" \
     --menu "Select a PSX game to download:" 15 50 10 "${menu[@]}" \
     2>&1 >/dev/tty)
-    
+
     # If Cancel is pressed, exit back to the GameDownloader.sh menu
     if [ $? -ne 0 ]; then
         return 1
     fi
-    
+
     # If a valid game is selected, start the download
     if [[ -n "$choice" ]]; then
         game_link=$(echo "${games[$choice-1]}" | cut -d ' ' -f 2-)
@@ -59,19 +59,19 @@ download_psx_game() {
     local game_link="$1"
     local file_name=$(basename "$game_link")
     local destination="/userdata/roms/psx/$file_name"
-    
+
     # Check if the game already exists
     if [ -f "$destination" ]; then
         dialog --msgbox "Game $file_name already exists in the PSX folder." 6 40
         return
     fi
-    
+
     # Show a download confirmation
     dialog --yesno "Do you want to download $file_name?" 6 40
     if [ $? -eq 0 ]; then
         # Start downloading the file using curl
         curl -L "$game_link" -o "$destination"
-        
+
         if [ $? -eq 0 ]; then
             dialog --msgbox "$file_name has been successfully downloaded." 6 40
         else
