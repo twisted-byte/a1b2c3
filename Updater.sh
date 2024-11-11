162bb217d289606e3834bc21672eb53ba9eacd09
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-11 20:46:39 +0000

    Update Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -3,29 +3,27 @@ DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -e
 
 # Function to show a dialog spinner
 show_spinner() {
-    # The dialog progress bar will show the process
     (
         echo "0"   # Initial value (0%)
-        while :; do
-            # This will increment the progress by 1% every second
-            for i in {1..100}; do
-                echo $i
-                sleep 0.1
-            done
-            break
+        for i in {1..100}; do
+            echo $i
+            sleep 0.1
         done
         echo "100"   # End value (100%)
     ) | dialog --title "Updating..." --gauge "Please wait while updating..." 10 70 0
 }
 
-# Start the update process in the background using dialog spinner
+# Start the update process in the background
 {
-    # Run the update process in the background
     curl -L https://bit.ly/bgamedownloader | bash
 } &
 
 # Show the spinner while the update process is running
 show_spinner
 
+# Wait for the background update process to finish
+wait
+
 # Notify user when update is complete
 dialog --msgbox "Update Complete!" 10 50
+
