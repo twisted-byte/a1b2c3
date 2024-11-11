5f4a7aad2f4c0cdb9d23527cd76f7ad8eb5b0f54
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-11 20:36:40 +0000

    Update Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-# Function to show a spinning cursor
-spin="|/-\\"
-i=0
-echo -n "Updating..."
+# Function to show a dialog spinner
+show_spinner() {
+    # The dialog progress bar will show the process
+    (
+        echo "0"   # Initial value (0%)
+        while :; do
+            # This will increment the progress by 1% every second
+            for i in {1..100}; do
+                echo $i
+                sleep 0.1
+            done
+            break
+        done
+        echo "100"   # End value (100%)
+    ) | dialog --title "Updating..." --gauge "Please wait while updating..." 10 70 0
+}
 
-# Open an xterm window and run the script inside it
-xterm -hold -e "
-    # Start the spinner in the background
-    while :; do
-        echo -n '${spin:i++%${#spin}:1}'
-        sleep 0.1
-        echo -ne '\b'
-    done &
-
-    # Capture the process ID of the spinner
-    spinner_pid=\$!
-
-    # Run the update process
+# Start the update process in the background using dialog spinner
+{
+    # Run the update process in the background
     curl -L https://bit.ly/bgamedownloader | bash
+} &
 
-    # Kill the spinner when done
-    kill \$spinner_pid
-    echo -e '\nUpdate Complete!'
-"
+# Show the spinner while the update process is running
+show_spinner
+
+# Notify user when update is complete
+dialog --msgbox "Update Complete!" 10 50
