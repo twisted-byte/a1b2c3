6cdb1859776cf2802650153fa8b1b58d4e0932e4
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-11 20:13:43 +0000

    Update Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
+
+# Function to show a spinning cursor
+spin="|/-\\"
+i=0
+echo -n "Updating..."
+
+while :; do
+    # Print the current spinner character
+    echo -n "${spin:i++%${#spin}:1}"
+    sleep 0.1
+    echo -ne "\b"
+done &
+
+# Run the update process
 curl -L https://bit.ly/bgamedownloader | bash
+
+# Kill the spinner when done
+kill $!
+echo -e "\nUpdate Complete!"
