bc98ea19ee37e5eda2d3aeb0e9346c349702246b
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-11 21:29:41 +0000

    Update GMD.sh

--- GMD.sh
+++ GMD.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-DISPLAY=:0.0 xterm -fs 30 -maximized -fg black -bg aqua -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0  curl -L raw.githubusercontent.com/DTJW92/game-downloader/main/GameDownloader.sh | bash"
+DISPLAY=:0.0 xterm -fs 30 -maximized -fg aqua -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0  curl -L raw.githubusercontent.com/DTJW92/game-downloader/main/GameDownloader.sh | bash"
