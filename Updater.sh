a000fe1230a62779b347546ac60068faf20970c2
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-11 20:49:03 +0000

    Update Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0  
 
-# Function to show a dialog spinner
-show_spinner() {
-    (
-        echo "0"   # Initial value (0%)
-        for i in {1..100}; do
-            echo $i
-            sleep 0.1
-        done
-        echo "100"   # End value (100%)
-    ) | dialog --title "Updating..." --gauge "Please wait while updating..." 10 70 0
-}
+# Open xterm to run the update process in the background
+DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "
+    # Function to show a dialog spinner
+    show_spinner() {
+        (
+            echo '0'   # Initial value (0%)
+            for i in {1..100}; do
+                echo \$i
+                sleep 0.1
+            done
+            echo '100'   # End value (100%)
+        ) | dialog --title 'Updating...' --gauge 'Please wait while updating...' 10 70 0
+    }
 
-# Start the update process in the background
-{
-    curl -L https://bit.ly/bgamedownloader | bash
-} &
+    # Start the update process in the background
+    {
+        curl -L https://bit.ly/bgamedownloader | bash
+    } &
 
-# Show the spinner while the update process is running
-show_spinner
+    # Show the spinner while the update process is running
+    show_spinner
 
-# Wait for the background update process to finish
-wait
+    # Wait for the background update process to finish
+    wait
 
-# Notify user when update is complete
-dialog --msgbox "Update Complete!" 10 50
-
+    # Notify user when update is complete
+    dialog --msgbox 'Update Complete!' 10 50
+"
