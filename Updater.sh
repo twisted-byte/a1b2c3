f29c1bf58e2be94c759f5e13d82005cea86f9cb1
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-12 02:48:55 +0000

    Update Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Source the formatting.sh script to apply custom formatting
+source /userdata/system/game-downloader/formatting.sh
+
 # Open xterm to run the update process in the background
-DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "
+DISPLAY=:0.0 xterm -fs 30 -maximized -fg "$TEXT_COLOR" -bg "$BACKGROUND_COLOR" -fa "DejaVuSansMono" -en UTF-8 -e bash -c "
     # Function to show a dialog spinner
     show_spinner() {
         (
