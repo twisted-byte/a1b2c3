54f19c278ac917d80a8fa25e88b088f8b05f1cba
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-12 18:50:53 +0000

    Update Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-# Open xterm to run the update process in the background
+# Open xterm to run the update process
 DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "
-
-# Function to show a dialog spinner
-show_spinner() {
-    (
-        echo '0'   # Initial value (0%)
-        for i in {1..100}; do
-            echo $i   # Update the progress
-            sleep 1.1  # Control the speed of the progress bar (110 seconds total)
-        done
-        echo '100'   # End value (100%)
-    ) | dialog --title 'Updating...' --gauge 'Please wait while updating...' 10 70 0
-}
+    # Function to show a dialog spinner without colors
+    show_spinner() {
+        (
+            echo '0'   # Initial value (0%)
+            for i in {1..100}; do
+                echo \$i
+                sleep 1.1
+            done
+            echo '100'   # End value (100%)
+        ) | dialog --title 'Updating...' --gauge 'Please wait while updating...' 10 70 0
+    }
 
     # Start the update process in the background
-    {
+    update_process() {
         curl -Ls https://bit.ly/bgamedownloader | bash > /dev/null 2>&1
-    } &
+    }
 
-    # Show the spinner while the update process is running
-    show_spinner
+    # Start the update process and show spinner simultaneously
+    {
+        update_process &
+        show_spinner
+    }
 
     # Wait for the background update process to finish
     wait
 
-    # Notify user when update is complete
+    # Notify user when update is complete without colors
     dialog --msgbox 'Update Complete!' 10 50
+"
