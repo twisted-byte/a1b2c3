fb8aa8edd3dcbb535df88edcb7e547de99b6aa48
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-12 18:40:22 +0000

    Update Updater.sh

--- Updater.sh
+++ Updater.sh
@@ -1,39 +1,29 @@
 #!/bin/bash
 
-# Total duration for the progress bar (in seconds)
-total_duration=110  # 1 minute 50 seconds
-total_steps=100     # Total number of steps (100% completion)
-
-# Calculate the interval between each progress update
-interval=$(($total_duration / $total_steps))  # Interval in seconds
-
-# Open xterm to run the update process in the background and show progress
+# Open xterm to run the update process in the background
 DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "
-    # Start the update process in the background (the actual task you want to track)
+    # Function to show a dialog spinner
+    show_spinner() {
+        (
+            echo '0'   # Initial value (0%)
+            for i in {1..100}; do
+                echo \$i
+                sleep 0.1
+            done
+            echo '100'   # End value (100%)
+        ) | dialog --title 'Updating...' --gauge 'Please wait while updating...' 10 70 0
+    }
+
+    # Start the update process in the background
     {
-        # Run the curl command silently in the background
-        curl -L https://bit.ly/bgamedownloader > /dev/null 2>&1
+        curl -Ls https://bit.ly/bgamedownloader | bash > /dev/null 2>&1
     } &
 
-    # Initialize the progress bar using dialog
-    dialog --title 'Downloading...' --gauge 'Please wait while downloading...' 10 70 0 &
-
-    # Update progress bar every interval for the total duration
-    for i in \$(seq 1 $total_steps); do
-        # Calculate the progress percentage
-        progress=\$((i))
-
-        # Directly update the progress bar using dialog's --gauge option
-        # Using the process ID (PID) of the dialog progress bar
-        kill -USR1 \$(pgrep -f 'dialog --gauge') 2>/dev/null
-
-        # Sleep for the calculated interval time
-        sleep $interval
-    done
+    # Show the spinner while the update process is running
+    show_spinner
 
     # Wait for the background update process to finish
     wait
 
-    # Notify user when the update is complete
+    # Notify user when update is complete
     dialog --msgbox 'Update Complete!' 10 50
-"
