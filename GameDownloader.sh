bd49f75e43fcf639349f8a1ec2d14e92e5288ec8
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-13 05:25:34 +0000

    Update GameDownloader.sh

--- GameDownloader.sh
+++ GameDownloader.sh
@@ -12,45 +12,56 @@ DOWNLOAD_MANAGER_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/m
 UNINSTALL_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/uninstall.sh"
 
 # Main dialog menu with loop to keep the menu active until a valid choice is selected
-while true; do
-    dialog --clear --backtitle "Game Downloader" \
-           --title "Select a System" \
-           --menu "Choose an option:" 15 50 8 \
-           1 "PSX Downloader" \
-           2 "PS2 Downloader" \
-           3 "Dreamcast Downloader" \
-           4 "Run Updater" \
-           5 "Run Download Manager" \
-           6 "Uninstall Game Downloader" 2>/tmp/game-downloader-choice
+dialog --clear --backtitle "Game Downloader" \
+       --title "Select a System" \
+       --menu "Choose an option:" 15 50 8 \
+       1 "PSX Downloader" \
+       2 "PS2 Downloader" \
+       3 "Dreamcast Downloader" \
+       4 "Run Updater" \
+       5 "Run Download Manager" \
+       6 "Uninstall Game Downloader" 2>/tmp/game-downloader-choice
 
-    choice=$(< /tmp/game-downloader-choice)
-    rm /tmp/game-downloader-choice
+choice=$(< /tmp/game-downloader-choice)
+rm /tmp/game-downloader-choice
 
-    case $choice in
-        1)
-            bash <(curl -s "$PSX_MENU_URL")
-            ;;
-        2)
-            bash <(curl -s "$PS2_MENU_URL")
-            ;;
-        3)
-            bash <(curl -s "$DC_MENU_URL")
-            ;;
-        4)
-            bash <(curl -s "$UPDATER_URL")
-            ;;
-        5)
-            bash <(curl -s "$DOWNLOAD_MANAGER_URL")
-            ;;
-        6)
-            bash <(curl -s "$UNINSTALL_URL")
-            ;;
-        *)
-        dialog --msgbox "No valid option selected. Exiting." 10 50
+# Check if the user canceled the dialog (no choice selected)
+if [ $? -ne 0 ]; then
+    clear
+    break
+fi
+
+# Execute the corresponding action based on user choice
+case $choice in
+    1)
+        bash <(curl -s "$PSX_MENU_URL")
+        ;;
+    2)
+        bash <(curl -s "$PS2_MENU_URL")
+        ;;
+    3)
+        bash <(curl -s "$DC_MENU_URL")
+        ;;
+    4)
+        bash <(curl -s "$UPDATER_URL")
+        ;;
+    5)
+        bash <(curl -s "$DOWNLOAD_MANAGER_URL")
+        ;;
+    6)
+        bash <(curl -s "$UNINSTALL_URL")
+        ;;
+    *)
+        # Handle invalid choices
+        dialog --infobox "Invalid option selected. Exiting..." 10 50
+        sleep 2
+        clear
+        # No exit or break, just continue
         ;;
 esac
-# Clear screen on exit
+
+# Clear screen at the end
 clear
 
 # Run the curl command to reload the games (output suppressed)
-curl http://127.0.0.1:1234/reloadgames &> /dev/null"
+curl http://127.0.0.1:1234/reloadgames &> /dev/null
