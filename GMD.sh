64163c668982e38ea23923a15d319612d970fe0f
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-13 02:28:57 +0000

    Update GMD.sh

--- GMD.sh
+++ GMD.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-# Set the display environment variable
-export DISPLAY=:0.0
-
-xterm -fs 30 -maximized -fg black -bg aqua -fa "DejaVuSansMono" -en UTF-8 -e bash -c "
-    # Create an info box with the message 'Please wait...'
-    dialog --title 'Please wait...' --backtitle 'Loading...' --infobox 'Please wait while the game downloader is running.' 10 50 &
-
-    curl -L https://raw.githubusercontent.com/DTJW92/game-downloader/main/GameDownloader.sh | bash
-
-" 
+DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0  curl -L raw.githubusercontent.com/DTJW92/game-downloader/main/GameDownloader.sh | bash"
