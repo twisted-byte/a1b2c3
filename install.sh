57f457630c6e79582447071d2cd7acc4574bff0e
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-13 04:41:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -47,6 +47,16 @@ bash <(curl -s "$PS2_SCRAPER")
 # Download bkeys.txt and save it as GameDownloader.sh.keys in the Ports folder
 curl -L "https://raw.githubusercontent.com/DTJW92/game-downloader/main/bkeys.txt" -o /userdata/roms/ports/GameDownloader.sh.keys
 
+# Download autostart.sh and make it executable
+curl -L "https://raw.githubusercontent.com/DTJW92/game-downloader/main/autostart.sh" -o /userdata/system/game-downloader/autostart.sh
+
+# Convert autostart.sh to Unix format and set permissions
+dos2unix ~/autostart.sh
+chmod 777 ~/autostart.sh
+
+# Append the contents of autostart.sh to custom.ch
+cat ~/autostart.sh >> /userdata/system/custom.ch
+
 # Reload games to reflect changes
 curl http://127.0.0.1:1234/reloadgames &> /dev/null
 
