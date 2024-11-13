545c5025e458f2f1b2c17e2e07665cf72a2d8afe
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-13 03:44:14 +0000

    Update GMD.sh

--- GMD.sh
+++ GMD.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0  curl -L raw.githubusercontent.com/DTJW92/game-downloader/main/GameDownloader.sh | bash"
+
+# Set the display environment variable
+DISPLAY=:0.0
+
+# Define the local path to save GameDownloader.sh
+LOCAL_SCRIPT_PATH="/userdata/system/game-downloader/GameDownloader.sh"
+
+# Download the GameDownloader.sh script locally
+curl -L "https://raw.githubusercontent.com/DTJW92/game-downloader/main/GameDownloader.sh" -o "$LOCAL_SCRIPT_PATH"
+
+# Make the downloaded script executable
+chmod +x "$LOCAL_SCRIPT_PATH"
+
+# Open an xterm window and run the downloaded GameDownloader.sh script
+xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "
+    # Execute the GameDownloader.sh script
+    bash '$LOCAL_SCRIPT_PATH'
+    
+    # After the script finishes, delete the local copy of GameDownloader.sh
+    rm -f '$LOCAL_SCRIPT_PATH'
+"
