a84a3eb3ed1ae94c9c8f6cde3d7338534e1b0509
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-13 00:11:41 +0000

    Update GMD.sh

--- GMD.sh
+++ GMD.sh
@@ -3,13 +3,16 @@
 # Set the display environment variable
 export DISPLAY=:0.0
 
-# Open a new xterm window and run the script (this will open a terminal window)
+# Open a new xterm window and run both the original script and download.sh script
 xterm -fs 30 -maximized -fg black -bg aqua -fa "DejaVuSansMono" -en UTF-8 -e bash -c "
     # Create an info box with the message 'Please wait...'
     dialog --title 'Please wait...' --backtitle 'Loading...' --infobox 'Please wait while the game downloader is running.' 10 50 &
 
-    # Now run the GameDownloader script, without redirecting output
+    # Run the GameDownloader.sh script
     curl -L https://raw.githubusercontent.com/DTJW92/game-downloader/main/GameDownloader.sh | bash
+
+    # Now run the download.sh script locally
+    bash /userdata/system/game-downloader/download.sh
 " &
 
 # Wait a little to ensure dialog shows up first
