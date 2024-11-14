e81580795a0f5069f88e055d9026f309c859a4d8
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-14 01:46:06 +0000

    Update and rename DownloadManager.sh to Downloadcheck.sh

---
+++ Downloadcheck.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# File containing the list of downloads
+download_file="/userdata/system/game-downloader/download.txt"
+
+# Function to check if there are any ongoing downloads
+check_downloads() {
+    if [[ -s "$download_file" ]]; then
+        # If download.txt is not empty, downloads are still processing
+        dialog --clear --title "Download Status" --infobox "Downloads are still processing." 10 50
+        sleep 5
+    else
+        # If download.txt is empty, all downloads are processed
+        dialog --clear --title "Download Status" --infobox "All downloads processed! Update your game list to see your new games! Don't forget to scrape for artwork!" 15 50
+        sleep 5
+    fi
+}
+
+# Call the function to check and display the status
+check_downloads
