0d0f53f0d0276f0e44d7cd87f20723b2ce64f912
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-14 00:23:56 +0000

    Update psx-downloader-menu.sh

--- psx-downloader-menu.sh
+++ psx-downloader-menu.sh
@@ -1,3 +1,52 @@
+#!/bin/bash
+
+BASE_URL="https://myrient.erista.me/files/Internet%20Archive/chadmaster/chd_psx_eur/CHD-PSX-EUR/"
+DEST_DIR="/userdata/system/game-downloader/psxlinks"
+DOWNLOAD_DIR="/userdata/roms/psx"  # Update this to your desired download directory
+ALLGAMES_FILE="$DEST_DIR/AllGames.txt"  # File containing the full list of games with URLs
+
+# Ensure the download directory exists
+mkdir -p "$DOWNLOAD_DIR"
+
+# Function to display the game list and allow selection
+select_games() {
+    local letter="$1"
+    local file="$DEST_DIR/${letter}.txt"
+
+    if [[ ! -f "$file" ]]; then
+        dialog --infobox "No games found for letter '$letter'." 5 40
+        sleep 2
+        return
+    fi
+
+    # Read the list of games from the file and prepare the dialog input
+    local game_list=()
+    while IFS="|" read -r decoded_name encoded_url; do
+        game_list+=("$decoded_name" "" off)
+    done < "$file"
+
+    selected_games=$(dialog --title "Select Games" --checklist "Choose games to download" 25 70 10 \
+        "${game_list[@]}" 3>&1 1>&2 2>&3)
+
+    if [ -z "$selected_games" ]; then
+        return
+    fi
+
+    IFS=$'\n'
+    for game in $selected_games; do
+        # Split game by .chd to treat each game as a separate item
+        game_items=$(echo "$game" | sed 's/\.chd/.chd\n/g')
+        while IFS= read -r game_item; do
+            if [[ -n "$game_item" ]]; then
+                game_item_cleaned=$(echo "$game_item" | sed 's/[\\\"]//g' | sed 's/^[[:space:]]*//g' | sed 's/[[:space:]]*$//g')
+                if [[ -n "$game_item_cleaned" ]]; then
+                    download_game "$game_item_cleaned"
+                fi
+            fi
+        done <<< "$game_items"
+    done
+}
+
 # Function to download the selected game and send the link to the DownloadManager
 download_game() {
     local decoded_name="$1"
@@ -54,3 +103,11 @@ while true; do
         break
     fi
 done
+
+# Goodbye message
+echo "Goodbye!"
+
+# Run the curl command to reload the games
+curl http://127.0.0.1:1234/reloadgames
+
+curl -L raw.githubusercontent.com/DTJW92/game-downloader/main/GameDownloader.sh | bash
