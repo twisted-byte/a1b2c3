e1bd8aea3b9cc3012b7201cc0656ccab133d6ec3
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-14 00:17:15 +0000

    Update psx-downloader-menu.sh

--- psx-downloader-menu.sh
+++ psx-downloader-menu.sh
@@ -1,52 +1,3 @@
-#!/bin/bash
-
-BASE_URL="https://myrient.erista.me/files/Internet%20Archive/chadmaster/chd_psx_eur/CHD-PSX-EUR/"
-DEST_DIR="/userdata/system/game-downloader/psxlinks"
-DOWNLOAD_DIR="/userdata/roms/psx"  # Update this to your desired download directory
-ALLGAMES_FILE="$DEST_DIR/AllGames.txt"  # File containing the full list of games with URLs
-
-# Ensure the download directory exists
-mkdir -p "$DOWNLOAD_DIR"
-
-# Function to display the game list and allow selection
-select_games() {
-    local letter="$1"
-    local file="$DEST_DIR/${letter}.txt"
-
-    if [[ ! -f "$file" ]]; then
-        dialog --infobox "No games found for letter '$letter'." 5 40
-        sleep 2
-        return
-    fi
-
-    # Read the list of games from the file and prepare the dialog input
-    local game_list=()
-    while IFS="|" read -r decoded_name encoded_url; do
-        game_list+=("$decoded_name" "" off)
-    done < "$file"
-
-    selected_games=$(dialog --title "Select Games" --checklist "Choose games to download" 25 70 10 \
-        "${game_list[@]}" 3>&1 1>&2 2>&3)
-
-    if [ -z "$selected_games" ]; then
-        return
-    fi
-
-    IFS=$'\n'
-    for game in $selected_games; do
-        # Split game by .chd to treat each game as a separate item
-        game_items=$(echo "$game" | sed 's/\.chd/.chd\n/g')
-        while IFS= read -r game_item; do
-            if [[ -n "$game_item" ]]; then
-                game_item_cleaned=$(echo "$game_item" | sed 's/[\\\"`]//g' | sed 's/^[[:space:]]*//g' | sed 's/[[:space:]]*$//g')
-                if [[ -n "$game_item_cleaned" ]]; then
-                    download_game "$game_item_cleaned"
-                fi
-            fi
-        done <<< "$game_items"
-    done
-}
-
 # Function to download the selected game and send the link to the DownloadManager
 download_game() {
     local decoded_name="$1"
@@ -75,57 +26,31 @@ download_game() {
 
     # Append the decoded name, URL, and folder to the DownloadManager.txt file
     echo "$decoded_name_cleaned|$game_url|$DOWNLOAD_DIR" >> "/userdata/system/game-downloader/download.txt"
-    dialog --infobox "'$decoded_name_cleaned' link added to download list." 5 40
-    sleep 2
+    
+    # Collect the added game
+    added_games+=("$decoded_name_cleaned")
 }
-
-# Function to show the letter selection menu
-select_letter() {
-    letter_list=$(ls "$DEST_DIR" | grep -oP '^[a-zA-Z#]' | sort | uniq)
-
-    menu_options=()
-    while read -r letter; do
-        menu_options+=("$letter" "$letter")
-    done <<< "$letter_list"
-
-    selected_letter=$(dialog --title "Select a Letter" --menu "Choose a letter" 25 70 10 \
-        "${menu_options[@]}" 3>&1 1>&2 2>&3)
-
-    if [ -z "$selected_letter" ]; then
-        return 1
-    fi
-
-    select_games "$selected_letter"
-}
-
-# Initialize an array to hold skipped games
-skipped_games=()
 
 # Main loop to process selected games
 while true; do
     select_letter
+
+    # Show a single message if any games were added to the download list
+    if [ ${#added_games[@]} -gt 0 ]; then
+        dialog --msgbox "Your selection has been added to the download list! Check download status and once it's complete, reload your games list to see the new games!" 10 50
+        # Clear the added games list
+        added_games=()
+    fi
+
     # Display skipped games message if there are any skipped games
     if [ ${#skipped_games[@]} -gt 0 ]; then
-        # Format the skipped games list with each game on a new line
         skipped_games_list=$(printf "%s\n" "${skipped_games[@]}" | sed 's/^/• /')
-
-        # Display the grouped skipped games in a message box
         dialog --msgbox "The following games already exist in the system and are being skipped:\n\n$skipped_games_list" 15 60
-        # Clear the skipped games list after displaying the message
         skipped_games=()
     fi
 
-    # Ask user if they want to continue after displaying skipped games
     dialog --title "Continue?" --yesno "Would you like to select some more games?" 7 50
     if [ $? -eq 1 ]; then
         break
     fi
 done
-
-# Goodbye message
-echo "Goodbye!"
-
-# Run the curl command to reload the games
-curl http://127.0.0.1:1234/reloadgames
-
-curl -L raw.githubusercontent.com/DTJW92/game-downloader/main/GameDownloader.sh | bash
