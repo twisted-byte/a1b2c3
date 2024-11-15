e7080900a5caa5bafebddba1a4db75d76430c822
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-15 03:39:06 +0000

    Update Main-Menu.sh

--- V2/Main-Menu.sh
+++ V2/Main-Menu.sh
@@ -1,64 +1,271 @@
 #!/bin/bash
 
-# URL for the combined game system menu script
+# URLs for external scripts
 COMBINED_MENU_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/Game-Menu.sh"
 UPDATER_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/Updater.sh"
 DOWNLOAD_MANAGER_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/Downloadcheck.sh"
 UNINSTALL_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/uninstall.sh"
 
-# Main dialog menu with loop to keep the menu active until a valid choice is selected
-dialog --clear --backtitle "Game Downloader" \
-       --title "Select a System" \
-       --menu "Choose an option:" 15 50 8 \
-       1 "PSX Downloader" \
-       2 "PS2 Downloader" \
-       3 "Dreamcast Downloader" \
-       4 "GBA Downloader" \
-       5 "Run Updater" \
-       6 "Status Checker" \
-       7 "Uninstall Game Downloader" 2>/tmp/game-downloader-choice
+# Function to display the Main Menu
+main_menu() {
+    local selection
+    selection=$(dialog --title "Main Menu" --menu "Select an option:" 15 50 8 \
+        1 "Select System Type" \
+        2 "Run Updater" \
+        3 "Status Checker" \
+        4 "Uninstall Game Downloader" \
+        5 "Exit" \
+        3>&1 1>&2 2>&3)
 
-choice=$(< /tmp/game-downloader-choice)
-rm /tmp/game-downloader-choice
+    case $selection in
+        1)
+            system_type_menu
+            ;;
+        2)
+            bash <(curl -s "$UPDATER_URL")
+            ;;
+        3)
+            bash <(curl -s "$DOWNLOAD_MANAGER_URL")
+            ;;
+        4)
+            bash <(curl -s "$UNINSTALL_URL")
+            ;;
+        5)
+            clear
+            dialog --infobox "Thank you for using Game Downloader! Any issues, message DTJW92 on Discord!" 10 50
+            sleep 3
+            exit 0
+            ;;
+        *)
+            main_menu
+            ;;
+    esac
+}
 
-# Check if the user canceled the dialog (no choice selected)
-if [ -z "$choice" ]; then
-    clear
-    dialog --infobox "Thank you for using Game Downloader! Any issues, message DTJW92 on Discord!" 10 50
-    sleep 3
-    exit 0  # Exit the script when Cancel is clicked or no option is selected
-fi
+# Function to display the System Type Menu
+system_type_menu() {
+    local system_type
+    system_type=$(dialog --title "Select System Type" --menu "Choose a system type" 15 50 4 \
+        1 "Sony" \
+        2 "Nintendo" \
+        3 "Microsoft" \
+        4 "Back to Main Menu" \
+        3>&1 1>&2 2>&3)
 
-# Execute the corresponding action based on user choice
-case $choice in
-    1)
-        bash <(curl -s "$COMBINED_MENU_URL") "PSX"
-        ;;
-    2)
-        bash <(curl -s "$COMBINED_MENU_URL") "PS2"
-        ;;
-    3)
-        bash <(curl -s "$COMBINED_MENU_URL") "Dreamcast"
-        ;;
-    4)
-        bash <(curl -s "$COMBINED_MENU_URL") "GBA"
-        ;;
-    5)
-        bash <(curl -s "$UPDATER_URL")
-        ;;
-    6)
-        bash <(curl -s "$DOWNLOAD_MANAGER_URL")
-        ;;
-    7)
-        bash <(curl -s "$UNINSTALL_URL")
-        ;;
-    *)
-        # Handle invalid choices
-        dialog --msgbox "Invalid option selected. Please try again." 10 50
-        clear
-        exit 0  # Exit the script if an invalid option is selected
-        ;;
-esac
+    case $system_type in
+        1)
+            game_system_menu "Sony"
+            ;;
+        2)
+            game_system_menu "Nintendo"
+            ;;
+        3)
+            game_system_menu "Microsoft"
+            ;;
+        4)
+            main_menu
+            ;;
+        *)
+            system_type_menu
+            ;;
+    esac
+}
 
-# Clear screen at the end
-clear
+# Function to display the Game System Menu for a specific system type
+game_system_menu() {
+    local system_type=$1
+    local system
+    case $system_type in
+        "Sony")
+            system=$(dialog --title "Select a Sony Game System" --menu "Choose a system" 15 50 4 \
+                1 "PSX" \
+                2 "PS2" \
+                3 "Back to System Type Menu" \
+                3>&1 1>&2 2>&3)
+            ;;
+        "Nintendo")
+            system=$(dialog --title "Select a Nintendo Game System" --menu "Choose a system" 15 50 4 \
+                1 "GBA" \
+                2 "Back to System Type Menu" \
+                3>&1 1>&2 2>&3)
+            ;;
+        "Microsoft")
+            system=$(dialog --title "Select a Microsoft Game System" --menu "Choose a system" 15 50 4 \
+                1 "Xbox" \
+                2 "Back to System Type Menu" \
+                3>&1 1>&2 2>&3)
+            ;;
+        *)
+            system_type_menu
+            ;;
+    esac
+
+    case $system in
+        1)
+            game_menu "$system_type" "PSX"
+            ;;
+        2)
+            if [ "$system_type" == "Sony" ]; then
+                game_menu "$system_type" "PS2"
+            elif [ "$system_type" == "Nintendo" ]; then
+                main_menu
+            elif [ "$system_type" == "Microsoft" ]; then
+                game_menu "$system_type" "Xbox"
+            fi
+            ;;
+        3)
+            system_type_menu
+            ;;
+        *)
+            game_system_menu "$system_type"
+            ;;
+    esac
+}
+
+# Function to display the Game Menu and select a letter or all games
+game_menu() {
+    local system_type=$1
+    local system=$2
+    local DEST_DIR DOWNLOAD_DIR ALLGAMES_FILE
+
+    # Set variables based on the system
+    case $system in
+        PSX)
+            DEST_DIR="/userdata/system/game-downloader/psxlinks"
+            DOWNLOAD_DIR="/userdata/roms/psx"
+            ALLGAMES_FILE="$DEST_DIR/AllGames.txt"
+            ;;
+        PS2)
+            DEST_DIR="/userdata/system/game-downloader/ps2links"
+            DOWNLOAD_DIR="/userdata/roms/ps2"
+            ALLGAMES_FILE="$DEST_DIR/AllGames.txt"
+            ;;
+        GBA)
+            DEST_DIR="/userdata/system/game-downloader/gbalinks"
+            DOWNLOAD_DIR="/userdata/roms/gba"
+            ALLGAMES_FILE="$DEST_DIR/AllGames.txt"
+            ;;
+        Xbox)
+            DEST_DIR="/userdata/system/game-downloader/xboxlinks"
+            DOWNLOAD_DIR="/userdata/roms/xbox"
+            ALLGAMES_FILE="$DEST_DIR/AllGames.txt"
+            ;;
+        *)
+            dialog --msgbox "Invalid system selected!" 10 50
+            return
+            ;;
+    esac
+
+    select_letter "$system" "$DEST_DIR" "$ALLGAMES_FILE" "$DOWNLOAD_DIR"
+}
+
+# Function to select a letter or "All Games" for a system
+select_letter() {
+    local system=$1
+    local DEST_DIR=$2
+    local ALLGAMES_FILE=$3
+    local DOWNLOAD_DIR=$4
+
+    letter_list=$(ls "$DEST_DIR" | grep -oP '^[a-zA-Z#]' | sort | uniq)
+
+    # Add "All" option to the menu
+    menu_options=("All" "All Games")
+
+    while read -r letter; do
+        menu_options+=("$letter" "$letter")
+    done <<< "$letter_list"
+
+    selected_letter=$(dialog --title "Select a Letter" --menu "Choose a letter or select 'All Games'" 25 70 10 \
+        "${menu_options[@]}" 3>&1 1>&2 2>&3)
+
+    if [ -z "$selected_letter" ]; then
+        return 1
+    elif [ "$selected_letter" == "All" ]; then
+        select_games "AllGames" "$ALLGAMES_FILE" "$DOWNLOAD_DIR"
+    else
+        select_games "$selected_letter" "$ALLGAMES_FILE" "$DOWNLOAD_DIR"
+    fi
+}
+
+# Function to display and allow selection of games from the list
+select_games() {
+    local letter="$1"
+    local file="$2"
+    local DOWNLOAD_DIR=$3
+
+    if [[ "$letter" == "AllGames" ]]; then
+        file="$ALLGAMES_FILE"
+    else
+        file="$DEST_DIR/${letter}.txt"
+    fi
+
+    if [[ ! -f "$file" ]]; then
+        dialog --infobox "No games found for selection '$letter'." 5 40
+        sleep 2
+        return
+    fi
+
+    # Read the list of games and prepare the dialog input
+    local game_list=()
+    while IFS="|" read -r decoded_name game_url; do
+        game_list+=("$decoded_name" "" off)
+    done < "$file"
+
+    selected_games=$(dialog --title "$system Games" --checklist "Choose games to download" 25 70 10 \
+        "${game_list[@]}" 3>&1 1>&2 2>&3)
+
+    if [ -z "$selected_games" ]; then
+        return
+    fi
+
+    IFS=$'\n'
+    for game in $selected_games; do
+        game_items=$(echo "$game" | sed 's/\.chd/.chd\n/g')
+        while IFS= read -r game_item; do
+            if [[ -n "$game_item" ]]; then
+                game_item_cleaned=$(echo "$game_item" | sed 's/[\\\"`]//g' | sed 's/^[[:space:]]*//g' | sed 's/[[:space:]]*$//g')
+                if [[ -n "$game_item_cleaned" ]]; then
+                    download_game "$game_item_cleaned" "$DOWNLOAD_DIR"
+                fi
+            fi
+        done <<< "$game_items"
+    done
+}
+
+# Function to download the selected game
+download_game() {
+    local decoded_name="$1"
+    local DOWNLOAD_DIR="$2"
+
+    decoded_name_cleaned=$(echo "$decoded_name" | sed 's/[\\\"`]//g' | sed 's/^[[:space:]]*//g' | sed 's/[[:space:]]*$//g')
+
+    # Check if the game already exists in the download directory
+    if [[ -f "$DOWNLOAD_DIR/$decoded_name_cleaned" ]]; then
+        skipped_games+=("$decoded_name_cleaned")
+        return
+    fi
+
+    # Check if the game is already in the download queue (download.txt)
+    if grep -q "$decoded_name_cleaned" "/userdata/system/game-downloader/download.txt"; then
+        skipped_games+=("$decoded_name_cleaned")
+        return
+    fi
+
+    # Find the game URL from the AllGames.txt file
+    game_url=$(grep -F "$decoded_name_cleaned" "$ALLGAMES_FILE" | cut -d '|' -f 2)
+
+    if [ -z "$game_url" ]; then
+        dialog --infobox "Error: Could not find download URL for '$decoded_name_cleaned'." 5 40
+        sleep 2
+        return
+    fi
+
+    # Append the decoded name, URL, and folder to the DownloadManager.txt file
+    echo "$decoded_name_cleaned|$game_url|$DOWNLOAD_DIR" >> "/userdata/system/game-downloader/download.txt"
+    
+    # Collect the added game
+    added_games+=("$decoded_name_cleaned")
+}
+
+# Start with the main menu
+main_menu
