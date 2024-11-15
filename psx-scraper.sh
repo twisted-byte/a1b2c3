3a2fe05ae28af5c161e22e6689de252f521b89d7
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-15 15:03:20 +0000

    Update psx-scraper.sh

--- psx-scraper.sh
+++ psx-scraper.sh
@@ -2,7 +2,7 @@
 
 # Base URL and destination directory
 BASE_URL="https://myrient.erista.me/files/Internet%20Archive/chadmaster/chd_psx_eur/CHD-PSX-EUR/
-DEST_DIR="/userdata/system/game-downloader/gbalinks
+DEST_DIR="/userdata/system/game-downloader/psxlinks"
 
 # Ensure the destination directory exists
 mkdir -p "$DEST_DIR"
