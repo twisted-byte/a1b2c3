fe4d0c0f7b85681c35006022a833b12aad86f047
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-16 01:42:59 +0000

    Create Updater.sh

---
+++ V2/Updater.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Open xterm to run the update process in the background
+DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "
+    # Function to show a dialog spinner
+    show_spinner() {
+        (
+            echo '0'   # Initial value (0%)
+            for i in {1..100}; do
+                echo \$i
+                sleep 0.8
+            done
+            echo '100'   # End value (100%)
+        ) | dialog --title 'Updating...' --gauge 'Please wait while updating...' 10 70 0
+    }
+
+    # Start the update process in the background
+    {
+        curl -Ls https://bit.ly/V2GD | bash > /dev/null 2>&1
+    } &
+
+    # Show the spinner while the update process is running
+    show_spinner
+
+    # Wait for the background update process to finish
+    wait
+
+    # Notify user when update is complete
+    dialog --msgbox 'Update Complete!' 10 50
+"
