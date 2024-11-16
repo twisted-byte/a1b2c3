de17559429c150f0e42d950b62049d0a234127a2
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-16 07:01:44 +0000

    Update MainMenu.sh

--- V3/MainMenu.sh
+++ V3/MainMenu.sh
@@ -4,19 +4,20 @@
 clear
 
 # URLs for external scripts
-UPDATER_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/V3/Updater.sh"
-DOWNLOAD_MANAGER_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/Downloadcheck.sh"
-UNINSTALL_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/V3/uninstall.sh"
-INSTALL_GAME_SYSTEMS_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/V3/installsystem.sh"  # New URL for install script
-
+UPDATER="https://raw.githubusercontent.com/DTJW92/game-downloader/main/V3/Updater.sh"
+DOWNLOAD_MANAGER="https://raw.githubusercontent.com/DTJW92/game-downloader/main/Downloadcheck.sh"
+UNINSTALL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/V3/uninstall.sh"
+INSTALL_GAME_SYSTEMS="https://raw.githubusercontent.com/DTJW92/game-downloader/main/V3/installsystem.sh"
+SELECT_SYSTEM="https://raw.githubusercontent.com/DTJW92/game-downloader/main/V3/SystemMenu.sh"
 # Main dialog menu with loop to keep the menu active until a valid choice is selected
 dialog --clear --backtitle "Game Downloader" \
        --title "Select a System" \
        --menu "Choose an option:" 15 50 9 \  # Increased the number of options to 9
-       1 "Install Game Systems" \  # Move this option to the top
-       2 "Run Updater" \
-       3 "Status Checker" \
-       4 "Uninstall Game Downloader" \
+       1 "Install Game Systems" \ 
+       2 "Select a Game System" \ 
+       3 "Run Updater" \
+       4 "Status Checker" \
+       5 "Uninstall Game Downloader" \
        2>/tmp/game-downloader-choice
 
 choice=$(< /tmp/game-downloader-choice)
@@ -34,15 +35,18 @@ fi
 case $choice in
     1)
         # Run the install game systems script
-        bash <(curl -s "$INSTALL_GAME_SYSTEMS_URL")  # Downloads and runs the installation script
+        bash <(curl -s "$INSTALL_GAME_SYSTEMS")  # Downloads and runs the installation script
         ;;
     2)
-        bash <(curl -s "$UPDATER_URL")
+        bash <(curl -s "$SELECT_SYSTEM")
         ;;
     3)
-        bash <(curl -s "$DOWNLOAD_MANAGER_URL")
+        bash <(curl -s "$UPDATER")
         ;;
     4)
+        bash <(curl -s "$DOWNLOAD_MANAGER")
+        ;;
+    5)
         bash <(curl -s "$UNINSTALL_URL")
         ;;
     *)
