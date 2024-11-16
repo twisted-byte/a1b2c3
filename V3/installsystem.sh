353518c64224f72eb35c8698aacdc2f30047f9a0
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-16 18:39:50 +0000

    Update installsystem.sh

--- V3/installsystem.sh
+++ V3/installsystem.sh
@@ -17,8 +17,8 @@ declare -A SCRAPERS
 SCRAPERS["PSX"]="https://raw.githubusercontent.com/DTJW92/game-downloader/main/scrapers/psx-scraper.sh"
 SCRAPERS["PS2"]="https://raw.githubusercontent.com/DTJW92/game-downloader/main/scrapers/ps2-scraper.sh"
 SCRAPERS["Xbox"]="https://raw.githubusercontent.com/DTJW92/game-downloader/main/scrapers/xbox-scraper.sh"
-SCRAPERS["Dreamcast"]="https://raw.githubusercontent.com/DTJW92/game-downloader/refs/heads/main/scrapers/dc-scraper.sh"
-SCRAPERS["GBA"]="https://raw.githubusercontent.com/DTJW92/game-downloader/refs/heads/main/scrapers/gba-scraper.sh"
+SCRAPERS["Dreamcast"]="https://raw.githubusercontent.com/DTJW92/game-downloader/main/scrapers/dc-scraper.sh"
+SCRAPERS["GBA"]="https://raw.githubusercontent.com/DTJW92/game-downloader/main/scrapers/gba-scraper.sh"
 
 # Create the menu dynamically based on the associative array
 MENU_OPTIONS=()
