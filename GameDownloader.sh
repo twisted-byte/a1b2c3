66599b05ba120e6d37523e98ee6ff74a551190e4
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-16 03:54:26 +0000

    Update GameDownloader.sh

--- GameDownloader.sh
+++ GameDownloader.sh
@@ -10,17 +10,17 @@ DC_MENU_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/dc-do
 UPDATER_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/Updater.sh"
 DOWNLOAD_MANAGER_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/Downloadcheck.sh"
 UNINSTALL_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/uninstall.sh"
+INSTALL_GAME_SYSTEMS_URL="https://raw.githubusercontent.com/DTJW92/game-downloader/main/install.sh"  # New URL for install script
 
 # Main dialog menu with loop to keep the menu active until a valid choice is selected
 dialog --clear --backtitle "Game Downloader" \
        --title "Select a System" \
-       --menu "Choose an option:" 15 50 8 \
-       1 "PSX Downloader" \
-       2 "PS2 Downloader" \
-       3 "Dreamcast Downloader" \
-       4 "Run Updater" \
-       5 "Status Checker" \
-       6 "Uninstall Game Downloader" 2>/tmp/game-downloader-choice
+       --menu "Choose an option:" 15 50 9 \  # Increased the number of options to 9
+       1 "Install Game Systems" \  # Move this option to the top
+       2 "Run Updater" \
+       3 "Status Checker" \
+       4 "Uninstall Game Downloader" \
+       2>/tmp/game-downloader-choice
 
 choice=$(< /tmp/game-downloader-choice)
 rm /tmp/game-downloader-choice
@@ -36,21 +36,16 @@ fi
 # Execute the corresponding action based on user choice
 case $choice in
     1)
-        bash <(curl -s "$PSX_MENU_URL")
+        # Run the install game systems script
+        bash <(curl -s "$INSTALL_GAME_SYSTEMS_URL")  # Downloads and runs the installation script
         ;;
     2)
-        bash <(curl -s "$PS2_MENU_URL")
-        ;;
-    3)
-        bash <(curl -s "$DC_MENU_URL")
-        ;;
-    4)
         bash <(curl -s "$UPDATER_URL")
         ;;
-    5)
+    3)
         bash <(curl -s "$DOWNLOAD_MANAGER_URL")
         ;;
-    6)
+    4)
         bash <(curl -s "$UNINSTALL_URL")
         ;;
     *)
