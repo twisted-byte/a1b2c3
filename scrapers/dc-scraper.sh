b8b1023b89be11f9783e8e5fe39edb0eb1fb7ea3
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-16 05:38:02 +0000

    Update dc-scraper.sh

--- scrapers/dc-scraper.sh
+++ scrapers/dc-scraper.sh
@@ -3,6 +3,8 @@
 # Base URL and destination directory
 BASE_URL="https://myrient.erista.me/files/Internet%20Archive/chadmaster/dc-chd-zstd-redump/dc-chd-zstd/"
 DEST_DIR="/userdata/system/game-downloader/links/Dreamcast"
+ROM_DIR="/userdata/roms/dreamcast"
+EXT=".chd"
 
 # Ensure the destination directory exists
 mkdir -p "$DEST_DIR"
@@ -24,13 +26,14 @@ clear_all_files
 # Fetch the page content
 page_content=$(curl -s "$BASE_URL")
 
-# Parse .chd links, decode them, and check for "(Europe)" or "(Europe, Australia)" and "(En)", excluding "(Demo)"
-echo "$page_content" | grep -oP '(?<=href=")[^"]*\.chd' | while read -r game_url; do
+# Parse .chd links, decode them, and check for Europe
+echo "$page_content" | grep -oP "(?<=href=\")[^\"]*${EXT}" | while read -r game_url; do
     # Decode the URL and check for the region tags and (En) in the decoded text
     decoded_name=$(decode_url "$game_url")
 
-    # Only process files that contain "(En)", "(Europe)" or "(Europe, Australia)", and do not contain "(Demo)"
-    if [[ "$decoded_name" =~ \(.*[Ee][Nn].*\) && ("$decoded_name" =~ \(.*Europe.*\) || "$decoded_name" =~ \(.*Europe,\ Australia.*\)) && ! "$decoded_name" =~ \(.*Demo.*\) ]]; then
+        if [[ "$decoded_name" =~ Europe ]]; then
+        # Process games matching the "Europe" criteria
+
         # Format the entry with backticks around the decoded name
         quoted_name="\`$decoded_name\`"
 
@@ -43,11 +46,11 @@ echo "$page_content" | grep -oP '(?<=href=")[^"]*\.chd' | while read -r game_url
         # Save to the appropriate letter-based file
         if [[ "$first_char" =~ [a-zA-Z] ]]; then
             first_char=$(echo "$first_char" | tr 'a-z' 'A-Z')
-            echo "$quoted_name|$BASE_URL$game_url" >> "$DEST_DIR/${first_char}.txt"
+            echo "$quoted_name|$BASE_URL$game_url|$ROM_DIR" >> "$DEST_DIR/${first_char}.txt"
         elif [[ "$first_char" =~ [0-9] ]]; then
-            echo "$quoted_name|$BASE_URL$game_url" >> "$DEST_DIR/#.txt"
+            echo "$quoted_name|$BASE_URL$game_url|$ROM_DIR" >> "$DEST_DIR/#.txt"
         else
-            echo "$quoted_name|$BASE_URL$game_url" >> "$DEST_DIR/other.txt"
+            echo "$quoted_name|$BASE_URL$game_url|$ROM_DIR" >> "$DEST_DIR/other.txt"
         fi
     fi
 done
