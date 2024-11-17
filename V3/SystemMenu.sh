9e57cf8e791e0cd5b60d0f7c2586843f5dc2a424
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-17 03:59:42 +0000

    Update SystemMenu.sh

--- V3/SystemMenu.sh
+++ V3/SystemMenu.sh
@@ -33,7 +33,7 @@ if [ ${#GAME_SYSTEMS[@]} -eq 0 ]; then
 fi
 
 # Add the option for the user to exit
-MENU_OPTIONS+=("0" "Exit")
+MENU_OPTIONS+=("0" "Return")
 
 # Main dialog menu with loop to keep the menu active until a valid choice is selected
 dialog --clear --backtitle "Game Downloader" \
@@ -55,13 +55,15 @@ fi
 # Execute the corresponding action based on user choice
 if [ "$choice" -eq 0 ]; then
     clear
-    dialog --infobox "Exiting Game Downloader..." 10 50
+    dialog --infobox "Returning to the main menu..." 10 50
     sleep 2
-    exit 0
+    exec /tmp/GameDownloader.sh  # Execute the main menu script
+    exit 0  # In case exec fails, exit the script
 else
     # Get the selected game system
     SELECTED_SYSTEM="${GAME_SYSTEMS[$((choice - 1))]}"
 fi
+
 
 # Define directories and files
 DEST_DIR="/userdata/system/game-downloader/links"
@@ -75,6 +77,7 @@ mkdir -p "$DOWNLOAD_DIR"
 skipped_games=()
 added_games=()
 
+# Function to display the game list and allow selection
 # Function to display the game list and allow selection
 select_games() {
     local letter="$1"
@@ -99,13 +102,19 @@ select_games() {
         game_list+=("$decoded_name" "" off)
     done < "$file"
 
+    # Add "Return" option at the top of the list
+    game_list=("Return" "${game_list[@]}")
+
+    # Show the game selection menu
     selected_games=$(dialog --title "Select Games" --checklist "Choose games to download" 25 70 10 \
         "${game_list[@]}" 3>&1 1>&2 2>&3)
 
-    if [ -z "$selected_games" ]; then
-        return
+    # If "Return" is selected, return to the letter selection menu
+    if [ "$selected_games" == "Return" ]; then
+        return 1  # Return to the letter selection menu
     fi
 
+    # Proceed with downloading the selected games
     IFS=$'\n'
     for game in $selected_games; do
         # Split game by .chd to treat each game as a separate item
@@ -120,6 +129,7 @@ select_games() {
         done <<< "$game_items"
     done
 }
+
 
 # Function to download the selected game and send the link to the DownloadManager
 download_game() {
@@ -163,14 +173,13 @@ download_game() {
 }
 
 
-# Function to show the letter selection menu with an "All Games" option
+# Function to show the letter selection menu with an "All Games" and "Return" options
 select_letter() {
     # Get a sorted list of the .txt files (A.txt, B.txt, etc.) in the selected game system
-    # Get a sorted list of the .txt files (A.txt, B.txt, etc.), excluding AllGames.txt
-letter_list=$(ls "$DEST_DIR/$SELECTED_SYSTEM"/*.txt | grep -v "AllGames.txt" | sed -E 's/\.txt$//' | sed 's/.*\///' | sort)
+    letter_list=$(ls "$DEST_DIR/$SELECTED_SYSTEM"/*.txt | grep -v "AllGames.txt" | sed -E 's/\.txt$//' | sed 's/.*\///' | sort)
 
-    # Add "All" option to the menu
-    menu_options=("All" "All Games")
+    # Add "Return" and "All" options to the menu
+    menu_options=("Return" "Back to System Selection" "All" "All Games")
 
     # Add each letter to the menu
     while read -r letter; do
@@ -181,9 +190,9 @@ letter_list=$(ls "$DEST_DIR/$SELECTED_SYSTEM"/*.txt | grep -v "AllGames.txt" | s
     selected_letter=$(dialog --title "Select a Letter" --menu "Choose a letter or select 'All Games'" 25 70 10 \
         "${menu_options[@]}" 3>&1 1>&2 2>&3)
 
-    # If a selection is made, proceed accordingly
-    if [ -z "$selected_letter" ]; then
-        return 1
+    # If "Return" is selected, return to the system selection menu
+    if [ "$selected_letter" == "Return" ]; then
+        return 1  # Return to the system selection
     elif [ "$selected_letter" == "All" ]; then
         # If "All Games" is selected, link to AllGames.txt
         select_games "AllGames"
@@ -192,6 +201,7 @@ letter_list=$(ls "$DEST_DIR/$SELECTED_SYSTEM"/*.txt | grep -v "AllGames.txt" | s
         select_games "$selected_letter"
     fi
 }
+
 
 # Main loop to process selected games
 while true; do
