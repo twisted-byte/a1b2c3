50e0eaff7acff8c61c55f1bd423379fba9a84b05
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-19 17:49:41 +0000

    Update SystemMenu.sh

--- V3/SystemMenu.sh
+++ V3/SystemMenu.sh
@@ -27,7 +27,7 @@ GAME_SYSTEMS=()
 MENU_OPTIONS=()
 
 # Define the predetermined order for the menu with internal system names
-MENUORDER=("PSX" "PS2" "PS3" "PSP" "PS Vita" "Xbox" "Xbox 360" "PC" "DOS" "Macintosh" "Game Boy" "Game Boy Color" "Game Boy Advance" "Nintendo DS" "NES" "SNES" "Nintendo 64" "GameCube" "Wii" "Game Gea[...]
+MENUORDER=("PSX" "PS2" "PS3" "PSP" "PS Vita" "Xbox" "Xbox 360" "PC" "DOS" "Macintosh" "Game Boy" "Game Boy Color" "Game Boy Advance" "Nintendo DS" "NES" "SNES" "Nintendo 64" "GameCube" "Wii" "Game Gear" "Dreamcast" "Saturn" "Mega Drive" "Master System" "32X" "SG-1000" "Game & Watch" "3DO" "Jaguar" "Lynx" "Neo Geo" "Neo Geo Pocket" "Neo Geo Pocket Color" "TurboGrafx-16" "TurboGrafx-CD" "PC Engine" "PC Engine CD" "SuperGrafx" "Amiga" "Amstrad CPC" "Atari 2600" "Atari 5200" "Atari 7800" "Atari 8-bit" "Atari ST" "Commodore 64" "Intellivision" "MSX" "MSX2" "Oric" "Sharp X68000" "TI-99/4A" "Vectrex" "WonderSwan" "WonderSwan Color" "ZX Spectrum")
 
 # Loop through the directories in /userdata/system/game-downloader/links in the predetermined order and add them to the menu
 index=1
@@ -87,10 +87,13 @@ mkdir -p "$DOWNLOAD_DIR"
 skipped_games=()
 added_games=()
 
-# Function to display the game list and allow selection
+# Function to display the game list and allow selection with pagination
 select_games() {
     local letter="$1"
     local file
+    local page_size=10  # Number of items per page
+    local current_page=1
+    local total_pages
 
     # Set file path based on the selection
     if [[ "$letter" == "AllGames" ]]; then
@@ -105,31 +108,78 @@ select_games() {
         return
     fi
 
-    # Read the list of games from the file and prepare the dialog input
+    # Read the list of games from the file
     local game_list=()
     while IFS="|" read -r decoded_name encoded_url; do
-        game_list+=("$decoded_name" "" off)
+        game_list+=("$decoded_name")
     done < "$file"
 
-    selected_games=$(dialog --title "Select Games" --checklist "Choose games to download" 25 70 10 \
-        "${game_list[@]}" 3>&1 1>&2 2>&3)
+    total_pages=$(((${#game_list[@]} + page_size - 1) / page_size))
 
-    if [ -z "$selected_games" ]; then
-        return
-    fi
+    while true; do
+        local start_index=$(( (current_page - 1) * page_size ))
+        local end_index=$(( start_index + page_size ))
+        local paged_game_list=("${game_list[@]:$start_index:$page_size}")
 
-    IFS=$'\n'
-    for game in $selected_games; do
-        # Split game by .chd to treat each game as a separate item
-        game_items=$(echo "$game" | sed 's/\.chd/.chd\n/g')
-        while IFS= read -r game_item; do
-            if [[ -n "$game_item" ]]; then
-                game_item_cleaned=$(echo "$game_item" | sed 's/[\\\"`]//g' | sed 's/^[[:space:]]*//g' | sed 's/[[:space:]]*$//g')
-                if [[ -n "$game_item_cleaned" ]]; then
-                    download_game "$game_item_cleaned"
+        local dialog_game_list=()
+        for game in "${paged_game_list[@]}"; do
+            dialog_game_list+=("$game" "" off)
+        done
+
+        selected_games=$(dialog --title "Select Games - Page $current_page of $total_pages" --checklist "Choose games to download" 25 70 10 \
+            "${dialog_game_list[@]}" 3>&1 1>&2 2>&3)
+
+        if [ -z "$selected_games" ]; then
+            break
+        fi
+
+        IFS=$'\n'
+        for game in $selected_games; do
+            # Split game by .chd to treat each game as a separate item
+            game_items=$(echo "$game" | sed 's/\.chd/.chd\n/g')
+            while IFS= read -r game_item; do
+                if [[ -n "$game_item" ]]; then
+                    game_item_cleaned=$(echo "$game_item" | sed 's/[\\\"`]//g' | sed 's/^[[:space:]]*//g' | sed 's/[[:space:]]*$//g')
+                    if [[ -n "$game_item_cleaned" ]]; then
+                        download_game "$game_item_cleaned"
+                    fi
                 fi
-            fi
-        done <<< "$game_items"
+            done <<< "$game_items"
+        done
+
+        # Navigation options
+        if (( current_page < total_pages )); then
+            next_page_option="Next Page"
+        else
+            next_page_option=""
+        fi
+
+        if (( current_page > 1 )); then
+            prev_page_option="Previous Page"
+        else
+            prev_page_option=""
+        fi
+
+        nav_choice=$(dialog --title "Navigation" --menu "Choose an option:" 15 50 12 \
+            "1" "Return" \
+            "2" "Next Page" \
+            "3" "Previous Page" 3>&1 1>&2 2>&3)
+
+        case $nav_choice in
+            1)
+                break
+                ;;
+            2)
+                if (( current_page < total_pages )); then
+                    ((current_page++))
+                fi
+                ;;
+            3)
+                if (( current_page > 1 )); then
+                    ((current_page--))
+                fi
+                ;;
+        esac
     done
 }
 
