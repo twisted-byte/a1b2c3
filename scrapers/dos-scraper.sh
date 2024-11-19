a483380db11fd7519c87c55bb1e94b1e0b52b51f
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-19 23:50:13 +0000

    Update dos-scraper.sh

--- scrapers/dos-scraper.sh
+++ scrapers/dos-scraper.sh
@@ -14,14 +14,6 @@ decode_url() {
     echo -n "$1" | sed 's/%/\\x/g' | xargs -0 printf "%b"
 }
 
-# Clear all the text files before writing new data
-clear_all_files() {
-    rm -f "$DEST_DIR"/*.txt
-}
-
-# Clear all text files before starting
-clear_all_files
-
 # Function to scrape a given base URL
 scrape_url() {
     local base_url="$1"
@@ -36,24 +28,24 @@ scrape_url() {
         # Get the first character of the decoded file name
         first_char="${decoded_name:0:1}"
         
-        # Check if the game already exists in AllGames.txt
-        if grep -q "$quoted_name" "$DEST_DIR/AllGames.txt"; then
+        # Determine the appropriate file based on the first character
+        if [[ "$first_char" =~ [a-zA-Z] ]]; then
+            first_char=$(echo "$first_char" | tr 'a-z' 'A-Z')
+            target_file="$DEST_DIR/${first_char}.txt"
+        elif [[ "$first_char" =~ [0-9] ]]; then
+            target_file="$DEST_DIR/#.txt"
+        else
+            target_file="$DEST_DIR/other.txt"
+        fi
+
+        # Check if the game already exists in the target file
+        if grep -q "$quoted_name" "$target_file"; then
             echo "Skipping $decoded_name as it already exists."
             continue
         fi
-        
-        # Append to AllGames.txt with both quoted decoded name and original URL
-        echo "$quoted_name|$base_url$game_url|$ROM_DIR" >> "$DEST_DIR/AllGames.txt"
-        
+
         # Save to the appropriate letter-based file
-        if [[ "$first_char" =~ [a-zA-Z] ]]; then
-            first_char=$(echo "$first_char" | tr 'a-z' 'A-Z')
-            echo "$quoted_name|$base_url$game_url|$ROM_DIR" >> "$DEST_DIR/${first_char}.txt"
-        elif [[ "$first_char" =~ [0-9] ]]; then
-            echo "$quoted_name|$base_url$game_url|$ROM_DIR" >> "$DEST_DIR/#.txt"
-        else
-            echo "$quoted_name|$base_url$game_url|$ROM_DIR" >> "$DEST_DIR/other.txt"
-        fi
+        echo "$quoted_name|$base_url$game_url|$ROM_DIR" >> "$target_file"
     done
 }
 
