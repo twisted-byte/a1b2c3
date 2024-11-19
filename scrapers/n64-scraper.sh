d257425d1e94c66bfa6c211509de8f20c1406492
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-19 05:00:19 +0000

    Update n64-scraper.sh

--- scrapers/n64-scraper.sh
+++ scrapers/n64-scraper.sh
@@ -25,31 +25,28 @@ clear_all_files
 # Fetch the page content
 page_content=$(curl -s "$BASE_URL")
 
-# Parse .chd links, decode them, and check for Europe
+# Parse .chd links, decode them, and process all links
 echo "$page_content" | grep -oP "(?<=href=\")[^\"]*${EXT}" | while read -r game_url; do
-    # Decode the URL and check for the region tags and (En) in the decoded text
+    # Decode the URL
     decoded_name=$(decode_url "$game_url")
-    if [[ "$decoded_name" =~ Europe ]]; then
-        # Process games matching the "Europe" criteria
-        # Format the entry with backticks around the decoded name
-        quoted_name="\`$decoded_name\`"
-        # Get the first character of the decoded file name
-        first_char="${decoded_name:0:1}"
-        
-        # Append to AllGames.txt with both quoted decoded name and original URL
-        echo "$quoted_name|$BASE_URL$game_url|$ROM_DIR" >> "$DEST_DIR/AllGames.txt"
-        
-        # Save to the appropriate letter-based file
-        if [[ "$first_char" =~ [a-zA-Z] ]]; then
-            first_char=$(echo "$first_char" | tr 'a-z' 'A-Z')
-            echo "$quoted_name|$BASE_URL$game_url|$ROM_DIR" >> "$DEST_DIR/${first_char}.txt"
-        elif [[ "$first_char" =~ [0-9] ]]; then
-            echo "$quoted_name|$BASE_URL$game_url|$ROM_DIR" >> "$DEST_DIR/#.txt"
-        else
-            echo "$quoted_name|$BASE_URL$game_url|$ROM_DIR" >> "$DEST_DIR/other.txt"
-        fi
+    
+    # Format the entry with backticks around the decoded name
+    quoted_name="\`$decoded_name\`"
+    # Get the first character of the decoded file name
+    first_char="${decoded_name:0:1}"
+    
+    # Append to AllGames.txt with both quoted decoded name and original URL
+    echo "$quoted_name|$BASE_URL$game_url|$ROM_DIR" >> "$DEST_DIR/AllGames.txt"
+    
+    # Save to the appropriate letter-based file
+    if [[ "$first_char" =~ [a-zA-Z] ]]; then
+        first_char=$(echo "$first_char" | tr 'a-z' 'A-Z')
+        echo "$quoted_name|$BASE_URL$game_url|$ROM_DIR" >> "$DEST_DIR/${first_char}.txt"
+    elif [[ "$first_char" =~ [0-9] ]]; then
+        echo "$quoted_name|$BASE_URL$game_url|$ROM_DIR" >> "$DEST_DIR/#.txt"
+    else
+        echo "$quoted_name|$BASE_URL$game_url|$ROM_DIR" >> "$DEST_DIR/other.txt"
     fi
 done
 
 echo "Scraping complete!"
-
