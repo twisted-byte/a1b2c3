6583d6bc4a7c7f5a297673496a6f684514173e81
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-20 05:15:11 +0000

    Update search2.sh

--- V3/search2.sh
+++ V3/search2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Exit on error or undefined variable
+set -e
+set -u
+
 # Function to search for games and display results in a dialog checklist
 search_games() {
     game_name=$(dialog --inputbox "Enter game name to search:" 8 40 3>&1 1>&2 2>&3 3>&-)
@@ -12,41 +16,48 @@ search_games() {
     # Find and grep for the game name in all .txt files in subdirectories
     results=$(find . -type f -name "*.txt" -exec grep -Hn "$game_name" {} \;)
 
+    if [ -z "$results" ]; then
+        dialog --msgbox "No matching games found!" 8 40
+        clear
+        exit 1
+    fi
+
     # Prepare checklist items
-    checklist_items=()
+    checklist_file=$(mktemp)
     while IFS= read -r line; do
         file=$(echo "$line" | cut -d: -f1)
         lineno=$(echo "$line" | cut -d: -f2)
         gameline=$(echo "$line" | cut -d: -f3)
-        gamename=$(echo "$gameline" | cut -d'|' -f1)
-        checklist_items+=("$file:$lineno" "$gamename" "off" "$file")
+        gamename=$(echo "$gameline" | awk -F'|' '{print $1}')
+        echo "$file:$lineno \"$gamename\" off" >> "$checklist_file"
     done <<< "$results"
 
-    # Show checklist dialog
-    selected_games=$(dialog --checklist "Select games to download:" 15 50 8 "${checklist_items[@]}" 3>&1 1>&2 2>&3 3>&-)
+    # Show checklist dialog to select games
+    selected_games=$(dialog --checklist "Select games to save information:" 15 50 8 --file "$checklist_file" 3>&1 1>&2 2>&3 3>&-)
+    rm "$checklist_file"
 
-    # Process selected games
+    # Process selected games and save the information
     for game in $selected_games; do
-        download_game "$game"
+        save_game_info "$game"
     done
 
     clear
-    echo "Download process completed."
+    echo "Game information saved."
 }
 
-# Function to download selected games
-download_game() {
+# Function to save selected game information (no downloading)
+save_game_info() {
     file_lineno="$1"
     file=$(echo "$file_lineno" | cut -d: -f1)
     lineno=$(echo "$file_lineno" | cut -d: -f2)
 
     # Read the specific line from the file
     gameline=$(sed "${lineno}q;d" "$file")
-    gamename=$(echo "$gameline" | cut -d'|' -f1 | tr -d '`')
-    url=$(echo "$gameline" | cut -d'|' -f2)
-    destination=$(echo "$gameline" | cut -d'|' -f3)
+    gamename=$(echo "$gameline" | awk -F'|' '{print $1}')
+    url=$(echo "$gameline" | awk -F'|' '{print $2}')
+    destination=$(echo "$gameline" | awk -F'|' '{print $3}')
 
-    # Append to download.txt
+    # Save the game information to download.txt
     echo "$gamename|$url|$destination" >> download.txt
 }
 
