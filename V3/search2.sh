5adcb7cd3d3cfd995f6d55592a37222e6c35b174
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-11-22 18:37:34 +0000

    Update search2.sh

--- V3/search2.sh
+++ V3/search2.sh
@@ -60,7 +60,7 @@ folder=$(basename "$(dirname "$file_path")")  # Extract the folder name of the f
     done <<< "$results"
 
     # Show dialog checklist for the user to select games
-    selected_games=$(dialog --checklist "Select games to save information:" 15 70 8 "${checklist_items[@]}" 2>&1 >/dev/tty)
+    selected_games=$(dialog --checklist "Select games to save information:" 15 60 8 "${checklist_items[@]}" 2>&1 >/dev/tty)
 
     # If the user cancels or exits the checklist, the selected_games will be empty
     if [ -z "$selected_games" ]; then
