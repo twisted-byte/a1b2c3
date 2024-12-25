4cbca17a9da4eb2c02e437b4429f01d1e7c8c0a2
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-12-25 02:04:43 +0000

    Update Updater.sh

--- V3/Updater.sh
+++ V3/Updater.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set locale environment variables
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 # Open xterm to run the update process in the background
 DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "
     # Function to show a dialog spinner
