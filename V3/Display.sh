4e813444ef2ea19e3d6523dd8b80595e86ffbfbb
DTJW92 <96082560+DTJW92@users.noreply.github.com>
2024-12-26 00:23:14 +0000

    Update Display.sh

--- V3/Display.sh
+++ V3/Display.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
 
-# Set the display environment variable
-export DISPLAY=:0.0
-
-# Define the local path to save GameDownloader.sh
-LOCAL_SCRIPT_PATH="/tmp/GameDownloader.sh"
-
-# Download the GameDownloader.sh script locally
-curl -L "https://raw.githubusercontent.com/DTJW92/game-downloader/main/V3/MainMenu.sh" -o "$LOCAL_SCRIPT_PATH"
-
-# Make the downloaded script executable
-chmod +x "$LOCAL_SCRIPT_PATH"
-
 # Open an xterm window and run the downloaded GameDownloader.sh script
-DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0
-    # Execute the GameDownloader.sh script
-    bash '$LOCAL_SCRIPT_PATH'
-    
-    # After the script finishes, delete the local copy of GameDownloader.sh
-    rm -f '$LOCAL_SCRIPT_PATH'
+DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0 curl -L "https://raw.githubusercontent.com/DTJW92/game-downloader/main/V3/MainMenu.sh" | bash"
     
     # Reload the game list
-    curl http://127.0.0.1:1234/reloadgames
-"
+    curl http://127.0.0.1:1234/reloadgames"
