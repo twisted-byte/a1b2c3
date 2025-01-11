08986c1e2ecae323c6088cb92b5382d9b7dbe516
twisted-byte <twisted.dnb92@gmail.com>
2025-01-11 16:04:23 +0000

    Update Display.sh

--- V3/Display.sh
+++ V3/Display.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Open an xterm window and run the downloaded GameDownloader.sh script
-DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0 curl -L "https://raw.githubusercontent.com/DTJW92/game-downloader/main/V3/MainMenu.sh" | bash"
+DISPLAY=:0.0 xterm -fs 30 -maximized -fg white -bg black -fa "DejaVuSansMono" -en UTF-8 -e bash -c "DISPLAY=:0.0 curl -L "https://raw.githubusercontent.com/twisted-byte/a1b2c3/main/V3/MainMenu.sh" | bash"
     
     # Reload the game list
     curl http://127.0.0.1:1234/reloadgames"
